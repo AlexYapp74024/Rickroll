3f3e3ea56c8e01e0a836c56bda7c37c786c23e9f
AlexY74024 <alexy74024@gmail.com>
2025-01-01 00:50:23 +0800

    small change to bench mark build process

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -7,7 +7,7 @@
 
 <div class="flex min-h-screen items-center justify-center">
   <div class="flex flex-col items-center justify-center space-y-4">
-    <h1 class="text-white text-4xl font-bold">You've Been Rick Rolled !!!</h1>
+    <h1 class="text-white text-4xl font-bold">You've Been Rick Rolled !</h1>
     <img
       src="/rickroll.gif"
       alt="Never Gonna Give You Up!"
